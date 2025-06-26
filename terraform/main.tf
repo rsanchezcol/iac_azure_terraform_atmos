c664ec102cb9b0e4910e613277db904e5bb6e911
Ramon Sanchez Villanueva <55895233+rsanchezcol@users.noreply.github.com>
2025-06-26 16:18:52 -0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,6 +27,7 @@ resource "azurerm_resource_group" "rg" {
 # Create a Virtual Network
 resource "azurerm_virtual_network" "vnet" {
   name                = "${var.environment}-vnet"
+  location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   address_space       = ["10.${lookup({"DEV": "0", "STG": "1", "PROD": "2"}, var.environment)}.0.0/16"]
 }
@@ -131,7 +132,6 @@ resource "azurerm_lb" "lb" {
 # Create Backend Pool for Load Balancer
 resource "azurerm_lb_backend_address_pool" "backend_pool" {
   loadbalancer_id     = azurerm_lb.lb.id
-  resource_group_name = azurerm_resource_group.rg.name
   name                = "${var.environment}-backend-pool"
 }
 
@@ -140,10 +140,10 @@ resource "azurerm_network_interface" "nic" {
   name                = "${var.environment}-vm-nic"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  subnet_id           = azurerm_subnet.backend_subnet.id
 
   ip_configuration {
     name                          = "${var.environment}-ip-config"
+    subnet_id                     = azurerm_subnet.backend_subnet.id
     private_ip_address_allocation = "Dynamic"
   }
 }
