542fe8bf96a76b804d86b417def3a6e8a6f8f513
Ramon Sanchez Villanueva <55895233+rsanchezcol@users.noreply.github.com>
2025-06-26 16:53:00 -0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,32 +2,6 @@ provider "azurerm" {
   features {}
 }
 
-# Define Input Variables
-variable "environment" {
-  description = "The environment in which resources will be deployed (DEV, STG, PROD)"
-  type        = string
-  validation {
-    condition     = can(regex("^(DEV|STG|PROD)$", var.environment))
-    error_message = "Invalid environment! Must be one of: DEV, STG, PROD."
-  }
-}
-
-variable "vm_admin_password" {
-  description = "The admin password for the VM"
-  type        = string
-  sensitive   = true
-}
-
-variable "vm_size" {
-  description = "Size of the virtual machine"
-  default     = "Standard_DS1_v2"
-}
-
-variable "location" {
-  description = "Azure region where resources will be provisioned"
-  default     = "East US"
-}
-
 # Create a Resource Group
 resource "azurerm_resource_group" "rg" {
   name     = "rg-${var.environment}-secure-infra"
