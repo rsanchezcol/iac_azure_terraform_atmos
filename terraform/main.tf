10f91f89b7b54acfec01db4682ad4e2a9e55e56a
Ramon Sanchez Villanueva <55895233+rsanchezcol@users.noreply.github.com>
2025-06-26 22:42:13 -0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,11 @@
 terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+
   backend "azurerm" {
     resource_group_name  = "devops-secure-infra-rg"
     storage_account_name = "devopsterraformstateacct"
