5a0055ae80bbbbc6aed99251722bdf0ada421593
Ramon Sanchez Villanueva <55895233+rsanchezcol@users.noreply.github.com>
2025-06-27 01:35:27 -0500

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -71,16 +71,19 @@ variable "network_segment" {
     DEV = {
       address_space           = ["10.0.0.0/16"]
       backend_subnet_prefixes = ["10.0.1.0/24"]
+      bastion_subnet_prefixes = ["10.0.2.0/24"]
       source_address_prefixes = ["10.0.0.0/16"]
     }
     STG = {
       address_space           = ["10.1.0.0/16"]
       backend_subnet_prefixes = ["10.1.1.0/24"]
+      bastion_subnet_prefixes = ["10.1.2.0/24"]
       source_address_prefixes = ["10.1.0.0/16"]
     }
     PROD = {
       address_space           = ["10.2.0.0/16"]
       backend_subnet_prefixes = ["10.2.1.0/24"]
+      bastion_subnet_prefixes = ["10.2.2.0/24"]
       source_address_prefixes = ["10.2.0.0/16"]
     }
   }
