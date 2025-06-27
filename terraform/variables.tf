ef3091b9b97bd15c6e5f6af5dfae8329b314127e
Ramon Sanchez Villanueva <55895233+rsanchezcol@users.noreply.github.com>
2025-06-26 23:16:04 -0500

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -15,6 +15,49 @@ variable "vm_admin_password" {
   sensitive   = true
 }
 
+# Define OS disk properties
+variable "os_disk" {
+  description = "OS disk configuration"
+  type        = map(string)
+  default = {
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+}
+
+# Define network segmentation based on environments
+variable "network_segment" {
+  description = "Network segmentation by environment"
+  type        = map(map(any))
+  default = {
+    DEV = {
+      address_space           = ["10.0.0.0/16"]
+      backend_subnet_prefixes = ["10.0.0.0/16"]
+      public_subnet_prefixes  = ["10.0.0.0/16"]
+      source_address_prefixes = ["10.0.0.0/16"]
+    }
+    STG = {
+      address_space           = ["10.1.0.0/16"]
+      backend_subnet_prefixes = ["10.1.0.0/16"]
+      public_subnet_prefixes  = ["10.1.0.0/16"]
+      source_address_prefixes = ["10.1.0.0/16"]
+    }
+    PROD = {
+      address_space           = ["10.2.0.0/16"]
+      backend_subnet_prefixes = ["10.2.0.0/16"]
+      public_subnet_prefixes  = ["10.2.0.0/16"]
+      source_address_prefixes = ["10.2.0.0/16"]
+    }
+  }
+}
+
+# VM name
+variable "vm_name" {
+  default     = "vm-webserver1"
+  description = "Name of the virtual machine"
+}
+
 variable "vm_size" {
   description = "Size of the virtual machine"
   default     = "Standard_D2lds_v6"
