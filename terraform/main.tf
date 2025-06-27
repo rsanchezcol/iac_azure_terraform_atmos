07c03455cc3bce25fdf5da50a5386aa9c9bd91ea
Ramon Sanchez Villanueva <55895233+rsanchezcol@users.noreply.github.com>
2025-06-26 23:23:09 -0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -149,7 +149,7 @@ resource "azurerm_network_interface" "nic" {
 }
 
 resource "azurerm_virtual_machine" "vm" {
-  name                  = var.vm_name
+  name                  = "${var.vm_name}-${var.environment}"
   location              = azurerm_resource_group.rg.location
   resource_group_name   = azurerm_resource_group.rg.name
   network_interface_ids = [azurerm_network_interface.nic.id]
